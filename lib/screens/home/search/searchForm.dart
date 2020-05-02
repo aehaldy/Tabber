7ebfbc08179db5674299fe8571b3bc8a50a63a5a
Amanda Haldy <aehaldy@tutanota.com>
2020-05-02 15:06:19 -0400

    Opens search form screen on button press

---
+++ lib/screens/home/search/searchForm.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SearchForm extends StatefulWidget {
+  @override
+  _SearchFormState createState() => _SearchFormState();
+}
+
+class _SearchFormState extends State<SearchForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: RaisedButton(
+        child: Text('Return'),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      )
+    );
+  }
+}
